b9fed4cac078b1dee89c701922c491bc8bc40b70
JATIN YADAV <jatinyadav9710@gmail.com>
2024-08-16 02:33:06 +0530

    final

--- FrontEnd/E-CART/E-Cart/src/Components/ProductDetail.jsx
+++ FrontEnd/E-CART/E-Cart/src/Components/ProductDetail.jsx
@@ -2,33 +2,83 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { items } from "./Data";
 import Product from "./Product";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-const ProductDetail = () => {
+const ProductDetail = ({ cart, setCart }) => {
   const { id } = useParams();
   const [product, setProduct] = useState({});
-  const [relatedProducts, setRelatedProducts] = useState([])
+  const [relatedProducts, setRelatedProducts] = useState([]);
   useEffect(() => {
     const filterProduct = items.filter((product) => product.id == id);
     setProduct(filterProduct[0]);
-    const relatedProducts = items.filter((prod)=> prod.category === product.category)
-    setRelatedProducts(relatedProducts)
-  }, [id,product.category]);
+    const relatedProducts = items.filter(
+      (prod) => prod.category === product.category
+    );
+    setRelatedProducts(relatedProducts);
+  }, [id, product.category]);
+  const addToCart = (id, price, title, description, imgSrc) => {
+    const obj = {
+      id,
+      price,
+      title,
+      description,
+      imgSrc,
+    };
+    setCart([...cart, obj]);
+    console.log("Cart Element = ", cart);
+    toast.success("Item is Added To Cart", {
+      position: "top-right",
+      autoClose: 1500,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "colored",
+    });
+  };
 
   return (
     <>
-      <div className="container con">
-        <div className="img">
+      <ToastContainer
+        position="top-right"
+        autoClose={1500}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
+      <div className="container con my-4">
+        <div className="img" id="detailImage">
           <img src={product.imgSrc} alt="" />
         </div>
-        <div className="text-center">
+        <div className="text-centervb " style={{ color: "white" }}>
           <h1 className="card-title">{product.title}</h1>
           <p className="card-text">{product.description}</p>
           <button className="btn btn-primary mx-3">{product.price} ₹</button>
-          <button className="btn btn-warning">Add to Cart</button>
+          <button
+            onClick={() =>
+              addToCart(
+                product.id,
+                product.price,
+                product.title,
+                product.description,
+                product.imgSrc
+              )
+            }
+            className="btn btn-warning"
+          >
+            Add to Cart
+          </button>
         </div>
       </div>
-      <h1 className ="text-center">Related Products</h1>
-      <Product items={relatedProducts} />
+      <h1 className="text-center">Related Products</h1>
+      <Product cart={cart} setCart={setCart} items={relatedProducts} />
     </>
   );
 };
--- FrontEnd/E-CART/E-Cart/src/Components/SearchItem.jsx
+++ FrontEnd/E-CART/E-Cart/src/Components/SearchItem.jsx
@@ -1,26 +1,23 @@
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
-import { items } from './Data';
-import Product from './Product';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { items } from "./Data";
+import Product from "./Product";
 
-const SearchItem = () => {
-  const {term} = useParams();
+const SearchItem = ({ cart, setCart }) => {
+  const { term } = useParams();
   const [filterData, setFilterData] = useState([]);
-  
+
   useEffect(() => {
-    const filteredData = () =>{
-      const data = items.filter((p)=>p.title.toLowerCase().includes(term.toLowerCase()))
-      setFilterData(data)
-    }
+    const filteredData = () => {
+      const data = items.filter((p) =>
+        p.title.toLowerCase().includes(term.toLowerCase())
+      );
+      setFilterData(data);
+    };
     filteredData();
-    
-  }, [term])
-  
+  }, [term]);
 
-  
-  return (
-    <Product items = {filterData}/>
-  )
-}
+  return <Product cart={cart} setCart={setCart} items={filterData} />;
+};
 
-export default SearchItem
+export default SearchItem;
