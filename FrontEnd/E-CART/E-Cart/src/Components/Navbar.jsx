7458db587c6ee373c6b8559b80094afd3a2f26c0
JATIN YADAV <jatinyadav9710@gmail.com>
2024-08-16 02:32:57 +0530

    close to final(8/16/2024)
    some minor changes left

--- FrontEnd/E-CART/E-Cart/src/Components/Navbar.jsx
+++ FrontEnd/E-CART/E-Cart/src/Components/Navbar.jsx
@@ -1,23 +1,25 @@
 import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { items } from "./Data";
+import { FaCartShopping } from "react-icons/fa6";
 
-const Navbar = ({ setData }) => {
+const Navbar = ({ setData, cart }) => {
+  const location = useLocation();
   const navigate = useNavigate();
-  const [searchTerm, setSearchTerm] = useState("")
+  const [searchTerm, setSearchTerm] = useState("");
   const filterByCategory = (category) => {
     const element = items.filter((product) => product.category === category);
     setData(element);
   };
-  const filterByPrice = (price)=>{
-    const element = items.filter((product)=>product.price >=price);
-    setData(element)
-  }
-  const handleSumbit = (e)=>{
+  const filterByPrice = (price) => {
+    const element = items.filter((product) => product.price >= price);
+    setData(element);
+  };
+  const handleSumbit = (e) => {
     e.preventDefault();
-    navigate(`/search/${searchTerm}`)
-    setSearchTerm("")
-  }
+    navigate(`/search/${searchTerm}`);
+    setSearchTerm("");
+  };
   return (
     <>
       <header className="sticky-top">
@@ -26,39 +28,54 @@ const Navbar = ({ setData }) => {
             E-Cart
           </Link>
 
-          <form 
-          onSubmit={handleSumbit}
-          className="search-bar">
+          <form onSubmit={handleSumbit} className="search-bar">
             <input
-            value = {searchTerm}
-            onChange = {(e)=> setSearchTerm(e.target.value)} 
-            type="text" 
-            placeholder="Search Products" />
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+              type="text"
+              placeholder="Search Products"
+            />
           </form>
 
           <Link to={"/cart"} className="cart">
-            Cart
+            <button type="button" className="btn btn-primary position-relative">
+              <FaCartShopping style={{ fontSize: "1.5rem" }} />
+              <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                {cart.length}
+                <span className="visually-hidden">unread messages</span>
+              </span>
+            </button>
           </Link>
         </div>
-        <div className="nav-bar-wrapper">
-          <div className="items">Filter By {"->"}</div>
-          <div onClick={() => setData(items)} className="items">
-            No Filter
+        {location.pathname == "/" && (
+          <div className="nav-bar-wrapper">
+            <div className="items">Filter By {"->"}</div>
+            <div onClick={() => setData(items)} className="items">
+              No Filter
+            </div>
+            <div onClick={() => filterByCategory("mobiles")} className="items">
+              Mobiles
+            </div>
+            <div onClick={() => filterByCategory("laptops")} className="items">
+              Laptops
+            </div>
+            <div onClick={() => filterByCategory("tablets")} className="items">
+              Tablets
+            </div>
+            <div onClick={() => filterByPrice(29999)} className="items">
+              {">="} 29999
+            </div>
+            <div onClick={() => filterByPrice(49999)} className="items">
+              {">="} 49999
+            </div>
+            <div onClick={() => filterByPrice(69999)} className="items">
+              {">="} 69999
+            </div>
+            <div onClick={() => filterByPrice(89999)} className="items">
+              {">="} 89999
+            </div>
           </div>
-          <div onClick={() => filterByCategory("mobiles")} className="items">
-            Mobiles
-          </div>
-          <div onClick={() => filterByCategory("laptops")} className="items">
-            Laptops
-          </div>
-          <div onClick={() => filterByCategory("tablets")} className="items">
-            Tablets
-          </div>
-          <div onClick={()=>filterByPrice(29999)} className="items">{">="} 29999</div>
-          <div onClick={()=>filterByPrice(49999)} className="items">{">="} 49999</div>
-          <div onClick={()=>filterByPrice(69999)} className="items">{">="} 69999</div>
-          <div onClick={()=>filterByPrice(89999)} className="items">{">="} 89999</div>
-        </div>
+        )}
       </header>
     </>
   );
--- FrontEnd/E-CART/E-Cart/src/Components/Product.jsx
+++ FrontEnd/E-CART/E-Cart/src/Components/Product.jsx
@@ -1,27 +1,67 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-
-const Product = ({items}) => {
+const Product = ({ items, cart, setCart }) => {
+  const addToCart = (id, price, title, description, imgSrc) => {
+    const obj = {
+      id,
+      price,
+      title,
+      description,
+      imgSrc,
+    };
+    setCart([...cart, obj]);
+    console.log("Cart Element = ", cart);
+    toast.success("Item is Added To Cart", {
+      position: "top-right",
+      autoClose: 1500,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "colored",
+    });
+  };
   return (
     <>
-      <div className="contaner my-5">
+      <ToastContainer
+        position="top-right"
+        autoClose={1500}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
+      <div className="contaner my-5 mx-5">
         <div className="row">
           {items.map((product) => {
             return (
               <>
-                <div key={product.id} className="col-lg-4 col-md-6 my-3 text-center">
+                <div
+                  key={product.id}
+                  className="col-lg-4 col-md-6 my-3 text-center"
+                >
                   <div className="card" style={{ width: "18rem" }}>
-                    <Link to={`/product/${product.id}`} style={{
-                      display:'flex',
-                      justifyContent:'center',
-                      alignItems:'center'
-                    }}>
-                    <img
-                      src={product.imgSrc}
-                      className="card-img-top"
-                      alt="..."
-                    />
+                    <Link
+                      to={`/product/${product.id}`}
+                      style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                      }}
+                    >
+                      <img
+                        src={product.imgSrc}
+                        className="card-img-top"
+                        alt="..."
+                      />
                     </Link>
                     <div className="card-body">
                       <h5 className="card-title">{product.title}</h5>
@@ -29,7 +69,20 @@ const Product = ({items}) => {
                       <button className="btn btn-primary mx-3">
                         {product.price} ₹
                       </button>
-                      <button className="btn btn-warning">Add to Cart</button>
+                      <button
+                        onClick={() =>
+                          addToCart(
+                            product.id,
+                            product.price,
+                            product.title,
+                            product.description,
+                            product.imgSrc
+                          )
+                        }
+                        className="btn btn-warning"
+                      >
+                        Add to Cart
+                      </button>
                     </div>
                   </div>
                 </div>
