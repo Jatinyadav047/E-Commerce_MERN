1d51a7aae5324e0ef44730c94e9d48cef25c3c86
JATIN YADAV <jatinyadav9710@gmail.com>
2024-09-19 00:44:41 +0530

    1.0

---
+++ API/Routes/user.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import { login ,register ,users} from '../Controllers/user.js';
+
+const router = express.Router();
+
+// register user
+router.post('/register',register) //=> /api/user/register
+
+// login user
+router.post('/login',login)
+
+// get all user's
+router.get('/all',users) 
+
+export default router
--- API/server.js
+++ API/server.js
@@ -1,10 +1,24 @@
 import express from 'express'
-import mongoose from 'mongoose'
-const app = express()
+import mongoose from 'mongoose';
+import bodyParser from 'express'
+import userRouter from './Routes/user.js'
+import productRouter from './Routes/product.js'
+
+const app = express();
+app.use(bodyParser.json())
+
+// home testing route
+app.get('/',(req,res)=>res.json({messge:'This is home route'}))
+// user Router
+app.use('/api/user',userRouter)
+// product Router
+app.use('/api/product',productRouter)
+
 mongoose.connect(
     "mongodb+srv://jatinyadav9710:Rxn10aNCbGPtXA2l@cluster0.mlerpvm.mongodb.net/",{
-        dbName : "MERN_E_Commerce"
+      dbName:"MERN_E-Commerce"
     }
-).then(()=> console.log("Mongo Connected sucsessfully")).catch((err)=> console.log(err));
+  ).then(()=>console.log("MongoDB Connected Succssfully...!")).catch((err)=>console.log(err));
+
 const port = 1000;
-app.listen(port,()=>console.log(`server is running on port ${port}`))
+app.listen(port,()=>console.log(`Server is running on port ${port}`))
